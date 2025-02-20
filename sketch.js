ea9ebda1fb9300d4e275c0d161e4bc9eb26bebf5
danett <danett.pepito@mds.ac.nz>
2025-02-20 19:33:27 +1300

    Initial Code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let circleRadius = 20;
+let maxRadius = 200;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480);
+  background(190);
+  noStroke();
+  noLoop();
+  colorMode(HSB);
+  fill(random(255), 200, 60, 0.5);
 }
 
 function draw() {
-  background(220);
+
+  for(let i=0;i<12;i++){ 
+  circleRadius = random(5, maxRadius);
+  circle(random(width), height/2, circleRadius);
+  strokeWeight(10);
+  stroke(10);
+  }
+
 }
